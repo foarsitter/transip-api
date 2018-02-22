e42889e884b81795f3ec23303fc2381a3b60a0a0
Jelmer Draaijer <jelmerdraaijer@gmail.com>
2018-02-22 10:26:57 +0100

    Fixed lint errors, disabled CamelCaseErrors because of representation of the Transip_ objects and too-few-public-methods because the stubs don't have methods (yet).

--- transip/service/webhosting.py
+++ transip/service/webhosting.py
@@ -1,8 +1,17 @@
-from transip.client import Client, MODE_RO, MODE_RW
+"""
+Implementation of the WebhostingService API endpoint
+"""
+
+# pylint: disable=invalid-name, too-few-public-methods
+
 from suds.sudsobject import Object as SudsObject
+from transip.client import Client, MODE_RW
 
 
 class WebhostingPackage(SudsObject):
+    """
+    Transip_WebhostingPackage representation
+    """
 
     def __init__(self, name, description, price, renewalPrice):
         super(WebhostingPackage, self).__init__()
@@ -14,41 +23,40 @@ class WebhostingPackage(SudsObject):
 
 
 class WebHost(SudsObject):
+    """
+    Transip_WebHost representation
+    """
 
-    def __init__(self, domainName, cronjobs=None, mailBoxes=None, dbs=None, mailForwards=None, subDomains=None):
+    def __init__(self, domainName):
         super(WebHost, self).__init__()
 
         self.domainName = domainName
-        self.cronjobs = cronjobs
-        self.mailBoxes = mailBoxes
-        self.dbs = dbs
-        self.mailForwards = mailForwards
-        self.subDomains = subDomains
+        self.cronjobs = None
+        self.mailBoxes = None
+        self.dbs = None
+        self.mailForwards = None
+        self.subDomains = None
 
 
 class MailBox(SudsObject):
+    """
+    Transip_MailBox representation
+    """
+
     SPAMCHECKER_STRENGTH_AVERAGE = 'AVERAGE'
     SPAMCHECKER_STRENGTH_OFF = 'OFF'
     SPAMCHECKER_STRENGTH_LOW = 'LOW'
     SPAMCHECKER_STRENGTH_HIGH = 'HIGH'
 
-    address = None,
-    spamCheckerStrength = SPAMCHECKER_STRENGTH_AVERAGE,
-    maxDiskUsage = 20
-    hasVacationReply = None
-    vacationReplySubject = ''
-    vacationReplyMessage = ''
-
-    def __init__(self, address, spamCheckerStrength=SPAMCHECKER_STRENGTH_AVERAGE, maxDiskUsage=20, hasVacationReply='',
-                 vacationReplySubject='', vacationReplyMessage=''):
+    def __init__(self, address, maxDiskUsage=20):
         super(MailBox, self).__init__()
 
         self.address = address
-        self.spamCheckerStrength = spamCheckerStrength
+        self.spamCheckerStrength = MailBox.SPAMCHECKER_STRENGTH_AVERAGE
         self.maxDiskUsage = maxDiskUsage
-        self.hasVacationReply = hasVacationReply
-        self.vacationReplySubject = vacationReplySubject
-        self.vacationReplyMessage = vacationReplyMessage
+        self.hasVacationReply = ''
+        self.vacationReplySubject = ''
+        self.vacationReplyMessage = ''
 
     def __eq__(self, other):
         if isinstance(other, MailBox):
@@ -61,6 +69,9 @@ class MailBox(SudsObject):
 
 
 class MailForward(SudsObject):
+    """
+    Transip_MailForward representation
+    """
 
     def __init__(self, name, targetAddress):
         super(MailForward, self).__init__()
@@ -74,42 +85,82 @@ class MailForward(SudsObject):
         if hasattr(other, 'value'):
             return self.targetAddress == other.value.targetAddress and self.name == other.value.name
 
+        return self.name == other
+
 
 class WebhostingService(Client):
+    """
+    Transip_WebhostingService
+    """
 
     def __init__(self, *args, **kwargs):
+        """
+        Constructor
+        """
         super(WebhostingService, self).__init__('WebhostingService', *args, **kwargs)
 
     def get_webhosting_domain_names(self):
+        """
+        Transip_WebhostingService::getWebhostingDomainNames
+        """
         return self._simple_request('getWebhostingDomainNames')
 
     def get_available_packages(self):
+        """
+        Transip_WebhostingService::getAvailablePackages
+        """
         return self._simple_request('getAvailablePackages')
 
     def get_info(self, domain):
-        """ Get available Webhosting products when available """
+        """
+        Transip_WebhostingService::getInfo
+        """
         return self._simple_request('getInfo', domain)
 
     def get_available_upgrades(self, domain):
+        """
+        Transip_WebhostingService::getAvailableUpgrades
+        """
         return self._simple_request('getAvailableUpgrades', domain)
 
     def create_mailbox(self, domain, mailbox):
+        """
+        Transip_WebhostingService::createMailBox
+        """
         return self._simple_request('createMailBox', domain, mailbox, mode=MODE_RW)
 
     def set_mailbox_password(self, domain, mailbox, password):
+        """
+        Transip_WebhostingService::setMailBoxPassword
+        """
         return self._simple_request('setMailBoxPassword', domain, mailbox, password, mode=MODE_RW)
 
     def update_mailbox(self, domain, mailbox):
+        """
+        Transip_WebhostingService::modifyMailBox
+        """
         return self._simple_request('modifyMailBox', domain, mailbox, mode=MODE_RW)
 
     def delete_mailbox(self, domain, mailbox):
+        """
+        Transip_WebhostingService::deleteMailBox
+        """
         return self._simple_request('deleteMailBox', domain, mailbox, mode=MODE_RW)
 
     def create_mail_forward(self, domain, mailforward):
+        """
+        Transip_WebhostingService::createMailForward
+        """
         return self._simple_request('createMailForward', domain, mailforward, mode=MODE_RW)
 
     def update_mail_forward(self, domain, mailforward):
+        """
+        Transip_WebhostingService::modifyMailForward
+        """
         return self._simple_request('modifyMailForward', domain, mailforward, mode=MODE_RW)
 
     def delete_mail_forward(self, domain, mailforward):
+        """
+        Transip_WebhostingService::deleteMailForward
+        """
         return self._simple_request('deleteMailForward', domain, mailforward, mode=MODE_RW)
